f7687af5acaf816252412b8139ff8af460af3bb2
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-14 07:21:32 +0000

    Create playlist controller

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,90 @@
+class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: %i[ show edit update destroy ]
+
+  # GET /playlists or /playlists.json
+  def index
+    @playlists = Playlist.all
+  end
+
+  # GET /playlists/1 or /playlists/1.json
+  def show
+  end
+
+  # GET /playlists/new
+  def new
+    @playlist = Playlist.new
+  end
+
+  # GET /playlists/1/edit
+  def edit
+  end
+
+  # POST /playlists or /playlists.json
+  def create
+    # @playlist = Playlist.new(playlist_params)
+    song_ids = playlist_params[:song_ids]
+
+    if song_ids.blank?
+      create_single_playlist
+    else
+      create_multiple_playlists(song_ids)
+    end
+
+    respond_to do |format|
+      if @playlist.save
+        format.html { redirect_to playlist_url(@playlist), notice: "Playlist was successfully created." }
+        format.json { render :show, status: :created, location: @playlist }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @playlist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /playlists/1 or /playlists/1.json
+  def update
+    respond_to do |format|
+      if @playlist.update(playlist_params)
+        format.html { redirect_to playlist_url(@playlist), notice: "Playlist was successfully updated." }
+        format.json { render :show, status: :ok, location: @playlist }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @playlist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /playlists/1 or /playlists/1.json
+  def destroy
+    @playlist.destroy
+
+    respond_to do |format|
+      format.html { redirect_to playlists_url, notice: "Playlist was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def create_single_playlist
+      @playlist = Playlist.new(playlist_params.except(:song_ids))
+    end
+
+    def create_multiple_playlists(song_ids)
+      song_ids.each do |song_id|
+        @playlist = Playlist.new(playlist_params.except(:song_ids))
+        @playlist.song_id = song_id
+        @playlist.save
+      end
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_playlist
+      @playlist = Playlist.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def playlist_params
+      params.require(:playlist).permit(:name, :date, song_ids: [] )
+    end
+end
