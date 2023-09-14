fcfb536bc22698fad19b39689ce86681bd2161ce
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-14 07:20:27 +0000

    Run migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_151059) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_160802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.bigint "song_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_id"], name: "index_playlists_on_song_id"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_151059) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "playlists", "songs"
 end
