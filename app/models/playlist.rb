2bc645a319f5b5d2ac80e5584cc4b87ca1dcdf4d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-15 12:21:13 +0000

    Update playlist table

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,4 @@
 class Playlist < ApplicationRecord
-  belongs_to :song, foreign_key: 'song_id'
+  # belongs_to :song, foreign_key: 'song_id'
+  has_many :item, foreign_key: 'song_id
 end
--- db/migrate/20230913160802_create_playlists.rb
+++ db/migrate/20230913160802_create_playlists.rb
@@ -3,7 +3,6 @@ class CreatePlaylists < ActiveRecord::Migration[7.0]
     create_table :playlists do |t|
       t.string :name
       t.date :date
-      t.references :song, null: false, foreign_key: true
 
       t.timestamps
     end
