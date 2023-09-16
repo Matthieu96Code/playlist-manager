e9a19d17af8c46b66f6f2b0a0dd66a6e07b19e20
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-16 10:31:10 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :playlists
+  resources :playlists do
+    resources :songs
+  end
   resources :items
+
+  root "items#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
